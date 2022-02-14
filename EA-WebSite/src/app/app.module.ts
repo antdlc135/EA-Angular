71c3780eecfbcce6076963dd735080a0dfc65a9c
Antonio De Luca <90771034+antdlc135@users.noreply.github.com>
2022-02-14 03:06:15 +0100

    all but footer

--- EA-WebSite/src/app/app.module.ts
+++ EA-WebSite/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './home-page/main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { AsideComponent } from './aside/aside.component';
 import { NavComponent } from './nav/nav.component';
+import { MainHeaderComponent } from './home-page/main/main-header/main-header.component';
+import { MainBodyComponent } from './home-page/main/main-body/main-body.component';
+import { MainCardsComponent } from './home-page/main/main-cards/main-cards.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +19,9 @@ import { NavComponent } from './nav/nav.component';
     HeaderComponent,
     AsideComponent,
     NavComponent,
+    MainHeaderComponent,
+    MainBodyComponent,
+    MainCardsComponent,
   ],
   imports: [BrowserModule],
   providers: [],
